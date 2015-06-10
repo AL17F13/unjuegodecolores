bc209df977ace9f91d831ee43c09d8b012bb2724
AL17F13 <asa.benitez23@gmail.com>
2015-06-10 13:56:09 -0500

    trece
    otra es

--- js/acciones.js
+++ js/acciones.js
@@ -1,68 +1,49 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	var basedatos=window.sqlitePlugin. openDatabase({name: "coloresBD.db",createFromLocation:1});
-	
-	$('#btnconfigurar').on('tap',function(){
-		$('#txtnombre').val($('#jugador').text() );
-		
-	});
-	$('#btnguardar').on ('tap',function (){
-		
-		var nuevonombre = $('txtnombre').val();
-		basedatos.transaction (function(consulta){
-			consulta.executeSql("UPDATE Usuario SET NombreUsuario=? WHERE ClaveUsuario='1';",
-			[nuevonombre]);
-			
-		});
-		cargarnombrejugador(); 
-		
-	});
-	
-	function flash (boton)
-	{
-		boton.stop ().animate({opacity:'0.5'},{
-			duration:80,
-			complete:function(){
-				boton.stop().animate({opacity:'1'},
-				200);
-			}
-		});
-	}
-	
-	$('.cuadro').on('tap',function (){
-		flash ($(this)); 
-		audio.play($(this).attr('id'));
-		
-	});
-	
-	
-	
-	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX ('B1','audio/C.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B2','audio/D.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B3','audio/E.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B4','audio/F.mp3',function() {},
-	function(msg){alert("error "+msg);});
+	var basedatos= window.sqlitePlugin.openDatabase ({name: "ColoresBD.db", createFromLocation:1});
 	cargarnombrejugador();
 	function cargarnombrejugador()
 	{
-		
-		basedatos.transaction(function(ejecutador){
-			var sql="SELET NombreUsuario FROM Usuario";
-			ejecutar.executesql(sql,undefined,function(ejecutar,resultado){
-			var datosJugador=resultado.rows.item(0);
-			$('#jugador').text (datosJugador.NombreUsuario);
-			
-			
-			
-			});
-		
-		});
+		basedatos.transaction (function (ejecutar){
+			var sql="SELECT NombreUsuario FROM Usuario";
+			ejecutar. executeSql (sql, undefined,function(ejecutar, resultado){
+				var datosJugador=resultado.rows.item(0);
+				$('#jugador').text(datosJugador.NombreUsuario);
+	
+	
+	});
+	cargarnombrejugador();
+	});
 	}
+	$('#btnajustes').on('tap',function(){
+	$('#txtnombre').val($('#jugador').text());
+	});
+	$('#btnguardar').on('tap',function(){
+		var nuevonombre=$('#txtnombre').val();
+		basedatos.transaction(function(consulta){
+		consulta.executeSql("UPDATE Usuario SET NombreUsuario=? WHERE ClaveUsuario='1';",[nuevonombre]);
+		});
+	});
+function flash (boton){
+	boton.stop().animate({opacity:'0.5'},{
+		duration:80,
+		complete:function(){
+			boton.stop().animate({opacity:'1'},
+			200);
+		}
+	});
+}
+
+$('.cuadro').on('tap',function (){
+		flash($(this) );
+		audio.play( );
+});
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B2', 'audio/D.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B3', 'audio/E.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B4', 'audio/F.mp3', function(){}, function(msg){alert ("Error " + msg);});
 $('#btnjugar').on ('tap',function(){
 
 	var pantalla=$.mobile.getScreenHeight();
@@ -78,27 +59,30 @@ $('#btnjugar').on ('tap',function(){
 	
 	
 	
-	
 });//btnjugar
 
 
 //$('.cuadro').on ('vmousedown', function(){
-	
-	//$('#pantalla').append (quien ($ (this).attr('id')));
+	//$('#pantalla').append (quien ($(this).attr('id')));
 	//$(this).addClass('pulsado');
-	//});	
+		
+    //});	
+	
 	
 	//$('.cuadro').on ('vmouseup', function(){
 	//$(this).removeClass('pulsado');	
     //});	
 	
+	
+	function quien (q)
+ {
+	 audio.play(q);
+	 return q.substring(1);
+ }
 
+});//cuadro
 
-function quien (q)
-{
- audio.play(q);
- return q.substring(1);
-}
-});//
+ 
 });
+
 
